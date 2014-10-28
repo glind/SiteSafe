b49a9797823d030051a3ff0382ce66bf1cffc20c
Greg Lind <gwlind@gmail.com>
2014-10-28 06:00:46 -0700

    cleanup

--- htdocs/sitemonitor/settings/example-local.py
+++ htdocs/sitemonitor/settings/example-local.py
@@ -10,7 +10,7 @@ from base import *
 ########## MANAGER CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ('glind', 'glind@mercycorps.org'),
+    ('admin', 'admin@sitesafe.org'),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
@@ -38,7 +38,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'secres',                      # Or path to database file if using sqlite3.
         'USER': 'secres',                      # Not used with sqlite3.
-        'PASSWORD': 'secres01',                  # Not used with sqlite3.
+        'PASSWORD': 'password',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
